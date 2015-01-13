1644b2fcf1c40fdeb27d66bbe70d322cb926b2bf
Courtney Mitchell <courtney.mitchell@live.com>
2015-01-12 16:34:48 -0800

    arrays 00

--- 08-arrays/00_Array_Definition/challenge.js
+++ 08-arrays/00_Array_Definition/challenge.js
@@ -1,3 +1,9 @@
-module.exports.newArray = undefined;
+module.exports.newArray = function(w,x,y,z) {
+  return [w,x,y,z];
+};
 
-module.exports.firstAndLast = undefined;
+module.exports.firstAndLast = function(arr) {
+  var first = arr[0];
+  var last = arr[arr.length - 1];
+  return [first,last];
+};
