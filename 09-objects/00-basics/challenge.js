4bb83f92bf012c26a16860eafa2963fbf99a8041
Courtney Mitchell <courtney.mitchell@live.com>
2015-01-15 11:27:48 -0800

    objects lesson

--- 09-objects/00-basics/challenge.js
+++ 09-objects/00-basics/challenge.js
@@ -1,7 +1,37 @@
-module.exports.createCourse = undefined;
+module.exports.createCourse = function(courseTitle, courseDuration, courseStudents) {
+  var course = {
+    title: courseTitle,
+    duration: courseDuration,
+    students: courseStudents
+  };
+  return course;
+};
 
-module.exports.addProperty = undefined;
+module.exports.addProperty = function(object, newProp, newValue) {
+  if (!object[newProp]) {
+    object[newProp] = newValue;
+  }
+  return object;
+};
 
-module.exports.formLetter = undefined;
+module.exports.formLetter = function(letter) {
 
-module.exports.canIGet = undefined;
+  return "Hello " + letter.recipient + ",\n\n" + letter.msg + "\n\nSincerely,\n" + letter.sender;
+};
+
+module.exports.canIGet = function(item, money) {
+  var macBookAir = {price: "999"};
+  var macBookPro = {price: "1299"};
+  var macPro = {price: "2499"};
+  var appleSticker = {price: "1"};
+
+  var buyingOptions = {
+    "MacBook Air": macBookAir,
+    "MacBook Pro": macBookPro,
+    "Mac Pro": macPro,
+    "Apple Sticker": appleSticker
+  };
+
+  return (buyingOptions[item] != null && money >= buyingOptions[item].price);
+
+};
