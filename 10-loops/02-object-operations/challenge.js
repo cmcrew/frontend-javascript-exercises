81c9365326ddca8a6fa7ed644b8720d4b5548468
Courtney Mitchell <courtney.mitchell@live.com>
2015-01-15 14:45:18 -0800

    loops 02 exercises

--- 10-loops/02-object-operations/challenge.js
+++ 10-loops/02-object-operations/challenge.js
@@ -1,5 +1,27 @@
-module.exports.copy = undefined;
+module.exports.copy = function(obj) {
+  var result = {};
+  for (i in obj ) {
+    result[i] = obj[i];
+  }
+  return result;
+};
 
-module.exports.extend = undefined;
+module.exports.extend = function(dest, src) {
+  for (i in src) {
+    dest[i] = src[i]
+  }
+  return dest;
+};
 
-module.exports.hasElems = undefined;
+module.exports.hasElems = function(obj, arr) {
+  var keys = [];
+  for (i in obj) {
+    keys.push(i);
+  }
+  for (var i = 0; i < arr.length; i++) {
+    if (keys.indexOf(arr[i]) == -1) {
+      return false;
+    }
+  } 
+  return true;
+};
