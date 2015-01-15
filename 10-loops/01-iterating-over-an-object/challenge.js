6af02bea32453e30c40ed19f0ee912b52dbf677f
Courtney Mitchell <courtney.mitchell@live.com>
2015-01-15 14:20:58 -0800

    loops 01 exercises

--- 10-loops/01-iterating-over-an-object/challenge.js
+++ 10-loops/01-iterating-over-an-object/challenge.js
@@ -1,5 +1,23 @@
-module.exports.getKeys = undefined;
+module.exports.getKeys = function(obj) {
+  var result = [];
+  for (var k in obj ) {
+    result.push(k);
+  }
+  return result;
+};
 
-module.exports.getValues = undefined;
+module.exports.getValues = function(obj) {
+  var result = [];
+  for (var key in obj) {
+    result.push(obj[key]);
+  }
+  return result;
+};
 
-module.exports.objectToArray = undefined;
+module.exports.objectToArray = function(obj) {
+  var result = [];
+  for (key in obj) {
+    result.push(key + " is " + obj[key]);
+  }
+  return result;
+};
