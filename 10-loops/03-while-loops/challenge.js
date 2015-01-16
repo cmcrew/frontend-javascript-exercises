e83d1f2a663f2dc1ff81bff792d18b0b42d85fdb
Courtney Mitchell <courtney.mitchell@live.com>
2015-01-15 19:47:34 -0800

    loops 03 exercises

--- 10-loops/03-while-loops/challenge.js
+++ 10-loops/03-while-loops/challenge.js
@@ -1,4 +1,18 @@
-module.exports.stream = undefined;
+module.exports.stream = function(conditionalFn, actionFn) {
+  while(conditionalFn() == true) {
+    actionFn();
+  }
+};
 
-module.exports.sumNumbers = undefined;
+module.exports.sumNumbers = function(arr) {
+  var result = 0;
+  if (arr.length > 0) {
+    var i = 0;
+    while(i < arr.length) {
+      result += arr[i];
+      i++;
+    }
+  }
+  return result;
+};
 
